df21855190a198f9d0ad76b3613ec52d4a7a2a96
RaziHussain <94157368+RaziHussain@users.noreply.github.com>
2022-02-20 23:45:59 +0530

    Update DBMS_Lab4.sql

--- DBMS_Lab4.sql
+++ DBMS_Lab4.sql
@@ -52,22 +52,32 @@ rename table price to price_table;
 /*Calculate price charged for each passenger displaying Passenger name, Boarding City, 
 Destination City, Bus_Type, Price in the output*/
 
-Select psg.Passenger_name, psg.Boarding_city, psg.Destination_City, psg.Bus_Type, pt.price from passenger as psg
-join price_table as pt on psg.Distance = pt.Distance;
+SELECT
+    p.Passanger_name,
+    p.Boarding_city,
+    p.Destination_City,
+    p.Bus_type,
+    pt.Price
+FROM
+    TravelOnTheGo.Passanger p
+INNER JOIN TravelOnTheGo.price_table pt
+    ON pt.distance = p.distance
+    group by p.Passanger_name;
+    
 
 /*What are the passenger name/s and his/her ticket price who travelled in the Sitting bus 
 for a distance of 1000 KM s*/
 
-Select psg.Passenger_name, pt.Price from passenger as psg 
-join Price_table as pt on
+Select psg.Passanger_name, pt.Price from TravelOnTheGo.Passanger as psg 
+join TravelOnTheGo.Price_table as pt on
 psg.Bus_type = pt.Bus_type  
 where psg.Distance >1000 and psg.Bus_Type = 'Sitting';
 
 /*What will be the Sitting and Sleeper bus charge for Pallavi to travel from Bangalore to 
 Panaji?*/
 
-select psg.Passenger_name, psg.Boarding_city , psg.Destination_city ,
-psg.Boardng_city, pt.Bus_type, pt.Price FROM passenger as psg inner join price_table as pt USING (distance)
+select psg.Passanger_name, psg.Boarding_city , psg.Destination_city ,
+psg.Boarding_city, pt.Bus_type, pt.Price FROM TravelOnTheGo.Passanger as psg inner join TravelOnTheGo.price_table as pt USING (distance)
 where ((psg.Boarding_city='Bengaluru' and psg.Destination_city='Panaji')
 or (psg.Boarding_city='Panaji' and psg.Destination_city='Bengaluru'));
 
@@ -75,24 +85,24 @@ or (psg.Boarding_city='Panaji' and psg.Destination_city='Bengaluru'));
 */
 
 select distinct(Distance) 
-from passenger
+from TravelOnTheGo.Passanger
 order by Distance desc;
 
 /*Display the passenger name and percentage of distance travelled by that passenger from the total distance travelled by all passengers without using user variables*/
 
-select passenger_name, (distance/(SELECT sum(distance) from passenger))*100 as percentage
-from passenger;
+select passanger_name, (distance/(SELECT sum(distance) from TravelOnTheGo.Passanger))*100 as percentage
+from TravelOnTheGo.Passanger;
 
 /*Display the distance, price in three categories in table Price
 a) Expensive if the cost is more than 1000
 b) Average Cost if the cost is less than 1000 and greater than 500
 c) Cheap otherwise */
 
-SELECT passenger.distance, price_table.price,
+SELECT passanger.distance, price_table.price,
 CASE
 	WHEN price_table.price > 1000 THEN "Expensive"
     WHEN price_table.price < 1000 and price > 500 THEN "Average_Cost"
     ELSE "Cheap" 
 END as price_table_Catergory
-FROM passenger join price_table on passenger.distance = price_table.distance
-group by passenger.distance;
+FROM TravelOnTheGo.Passanger join TravelOnTheGo.price_table on Passanger.distance = price_table.distance
+group by passanger.distance;
